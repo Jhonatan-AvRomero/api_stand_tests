a436c33baae24bca316e75d5b3f96bec35ff1fb4
Jhonatan Avendaño Romero <jhonatanavrom@yahoo.com>
2024-02-01 00:14:03 -0600

    Mi primer commit

---
+++ sender_stand_request.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://f32c9b54-0266-4433-a039-62cf287c168c.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
